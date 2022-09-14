8ce29b55dedf7cd2a8ac8e414133daa4de65f705
Jacky <jaeeky.shao@gmail.com>
2022-09-15 01:38:27 +0200

    feat(subscription): add promo codes

---
+++ src/api/promotion-code/routes/promotion-code.ts
@@ -0,0 +1,7 @@
+/**
+ * promotion-code router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::promotion-code.promotion-code');
---
+++ src/api/promotion-code/services/promotion-code.ts
@@ -0,0 +1,7 @@
+/**
+ * promotion-code service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::promotion-code.promotion-code');
